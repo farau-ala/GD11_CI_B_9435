52ce1662e485b21b9d4a6155e840c30a5293c9df
Farau AlA <alfarau99@gmail.com>
2019-11-14 07:37:30 +0700

    first commit

--- application/models/UserModel.php
+++ application/models/UserModel.php
@@ -1,25 +1,25 @@
-<?php 
-defined('BASEPATH') OR exit('No direct script access allowed'); 
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-class UserModel extends CI_Model 
-{ 
-    private $table = 'users'; 
-    
-    
-    public $id; 
-    public $name; 
-    public $email; 
-    public $password; 
+class UserModel extends CI_Model
+{
+    private $table = 'users';
+
+
+    public $id;
+    public $name;
+    public $email;
+    public $password;
     public $rule = [ 
-        [ 
-            'field' => 'name', 
-            'label' => 'name', 
-            'rules' => 'required' 
-        ], 
-    ]; 
+        [
+            'field' => 'name',
+            'label' => 'name',
+            'rules' => 'required'
+        ],
+    ];
     public function Rules() { return $this->rule; }
-    
-    
+   
+
     public function getAll() { return 
         $this->db->get('data_mahasiswa')->result(); 
     } 
@@ -27,25 +27,25 @@ class UserModel extends CI_Model
         $this->name = $request->name; 
         $this->email = $request->email; 
         $this->password = password_hash($request->password, PASSWORD_BCRYPT); 
-        if($this->db->insert($this->table, $this)){ 
+        if($this->db->insert($this->table, $this)){
             return ['msg'=>'Berhasil','error'=>false];
-        } 
-        return ['msg'=>'Gagal','error'=>true]; 
-    } 
-    public function update($request,$id) { 
-        $updateData = ['email' => $request->email, 'name' =>$request->name]; 
-        if($this->db->where('id',$id)->update($this->table, $updateData)){ 
-            return ['msg'=>'Berhasil','error'=>false]; 
-        } 
-        return ['msg'=>'Gagal','error'=>true]; 
-    } 
-    public function destroy($id){ 
-        if (empty($this->db->select('*')->where(array('id' => $id))->get($this->table)->row())) return ['msg'=>'Id tidak ditemukan','error'=>true]; 
-        
-        if($this->db->delete($this->table, array('id' => $id))){ 
-            return ['msg'=>'Berhasil','error'=>false]; 
-        } 
+        }
         return ['msg'=>'Gagal','error'=>true];
     }
-} 
+    public function update($request,$id) { 
+        $updateData = ['email' => $request->email, 'name' =>$request->name];
+        if($this->db->where('id',$id)->update($this->table, $updateData)){
+            return ['msg'=>'Berhasil','error'=>false];
+        }
+        return ['msg'=>'Gagal','error'=>true];
+    }
+    public function destroy($id){
+        if (empty($this->db->select('*')->where(array('id' => $id))->get($this->table)->row())) return ['msg'=>'Id tidak ditemukan','error'=>true];
+        
+        if($this->db->delete($this->table, array('id' => $id))){
+            return ['msg'=>'Berhasil','error'=>false];
+        }
+        return ['msg'=>'Gagal','error'=>true];
+    }
+}
 ?>
